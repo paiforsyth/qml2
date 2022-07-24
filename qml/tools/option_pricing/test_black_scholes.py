643ccc7672a5c28604196e2525a7cda5d5d89fb2
Peter Forsyth <paiforsyth@hotmail.com>
2022-07-24 17:30:06 -0400

    new test

--- qml/tools/option_pricing/test_black_scholes.py
+++ qml/tools/option_pricing/test_black_scholes.py
@@ -21,3 +21,23 @@ vol:float
        strike=jnp.array([strike]), time_to_maturity=jnp.array([0]),risk_free_rate=risk_free_rate,vol=vol
    )
    np.testing.assert_allclose(price, jnp.maximum(underlying_price-strike,0.0), atol=1e-6, rtol=1e-6)
+
+
+@given(
+    time_to_maturity = st.floats(min_value=0.0, max_value=3.0, width=32),
+    strike= st.floats(min_value=0, max_value=10, width=32),
+    risk_free_rate = st.floats(min_value=0.0, max_value=0.5,width=32 ),
+    vol = st.floats(min_value=0.0, max_value=0.5, width=32)
+)
+def test_price_large_price(
+        time_to_maturity: float,
+        strike: float,
+        risk_free_rate: float,
+        vol: float
+):
+    underlying_price = 10.0**20
+    price = price_call(
+        underlying_price=jnp.array([underlying_price]),
+        strike=jnp.array([strike]), time_to_maturity=jnp.array([time_to_maturity]), risk_free_rate=risk_free_rate, vol=vol
+    )
+    np.testing.assert_allclose(price, underlying_price - strike, atol=1e-6, rtol=1e-6)
